8a029318938908b761edbf557825d328328632c0
blinbingo <ian.oddssports@gmail.com>
2025-04-19 12:46:07 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,12 @@
-
 export default function Home() {
   return (
-    <div className="background"></div>
+    <div className="background">
+      <header className="topo">
+        <div className="botoes">
+          <button className="btn-transparente">Entrar</button>
+          <button className="btn-verde">Cadastre-se</button>
+        </div>
+      </header>
+    </div>
   );
 }
