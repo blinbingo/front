80e140c11901a6184b7b20980197e79884d5dcdd
blinbingo <ian.oddssports@gmail.com>
2025-04-19 14:44:14 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,6 @@
 export default function Home() {
   return (
     <div className="background">
-      <aside className="menu-lateral">
-        <div className="menu-top">
-          <img src="/assets/icon-bingo.svg" alt="Bingo" />
-          <img src="/assets/icon-carta.svg" alt="Cartas" />
-        </div>
-        <div className="menu-bottom">
-          <img src="/assets/icon-contato.svg" alt="Contato" />
-        </div>
-      </aside>
-
       <header className="topo">
         <img src="/assets/logo.png" alt="Logo" className="logo" />
         <div className="botoes">
@@ -20,19 +10,25 @@ export default function Home() {
         </div>
       </header>
 
-      <main className="banner">
-        <div className="texto">
+      <main className="conteudo-central">
+        <div className="banner-texto">
           <h1>
-            JOGOS <span className="verde">EMOCIONANTES</span> E<br />
-            PRÊMIOS <span className="verde">INCRÍVEIS</span> SEM<br />
+            JOGOS <span className="verde">EMOCIONANTES</span> E <br />
+            PRÊMIOS <span className="verde">INCRÍVEIS</span> SEM <br />
             SAIR DE CASA!
           </h1>
           <p>Sua sorte está apenas a um clique de distância!</p>
         </div>
-        <div className="imagem">
+        <div className="banner-imagem">
           <img src="/assets/banner.png" alt="Banner" />
         </div>
       </main>
+
+      <div className="icones-flutuantes">
+        <img src="/assets/icon-bingo.svg" alt="Bingo" />
+        <img src="/assets/icon-carta.svg" alt="Cartas" />
+        <img src="/assets/icon-contato.svg" alt="Contato" className="contato" />
+      </div>
     </div>
   );
 }
