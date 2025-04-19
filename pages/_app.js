e0a84a5417fefa86e7881328d0399bd7d9b551f3
blinbingo <ian.oddssports@gmail.com>
2025-04-19 12:42:24 -0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 
-import '@/styles/style.css';
+import '../styles/style.css'; // ✅ FUNCIONA SEM PRECISAR DE NADA EXTRA
+
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
